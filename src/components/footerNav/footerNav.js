9422c53b7fc7b3550f9675f404df0504eb962047
rida-123 <ridashoaib666@gmail.com>
2022-05-11 12:07:49 -0700

    add sections

--- src/components/footerNav/footerNav.js
+++ src/components/footerNav/footerNav.js
@@ -5,10 +5,10 @@ const FooterNav=()=>{
         <div className='footerNav' >
             <div className='container-fluid'>
                 <div className='row'>
-                    <div className='col-sm-3 col-lg-3 col-md-3'>
+                    <div className='col-sm-12 col-lg-3 col-md-12'>
 <img src="kicksale-logo.png" className='immg'></img>
                     </div>
-                    <div className='col-sm-6 col-lg-6 col-md-6' style={{marginTop:"30px"}}>
+                    <div className='col-sm-12 col-lg-6 col-md-12' style={{marginTop:"30px"}}>
                         <div className='navFooter'>
                         <span>
                             <a href="#">Home</a>
@@ -27,7 +27,7 @@ const FooterNav=()=>{
                         </span>
                         </div>
                         </div>
-                        <div className='col-sm-4\3 col-lg-3 col-md-3' style={{marginTop:"10px", textAlign:"right"}}>
+                        <div className='col-sm-12 col-lg-3 col-md-12' style={{marginTop:"10px", textAlign:"right"}}>
                             <div className='icons'>
                                 <a href="#"><img src="vector1.png"></img></a>
                                 <a href="#"><img src="vector2.png"></img></a>
@@ -44,10 +44,10 @@ const FooterNav=()=>{
         <br></br>
         <div className='container-fluid' style={{backgroundColor:"#090713",paddingBottom:"20px"}}>
         <div className='row'>
-            <div className='col-sm-4 col-md-4 col-lg-4'>
+            <div className='col-sm-12 col-md-12 col-lg-4'>
                 <p className='para11'>Copyright Kicksale © 2022. All rights reserved</p>
             </div>
-            <div className='col-sm-8 col-md-8 col-lg-8' style={{textAlign:"right"}}>
+            <div className='col-sm-12 col-md-12 col-lg-8' style={{textAlign:"right"}}>
             <div className='navFooter1'>
                         <span>
                             <a href="#">Privacy Policy</a>
