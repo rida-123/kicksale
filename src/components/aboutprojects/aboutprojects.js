c7e3e2b19da3dda0e7c2ccfee452fc7c0ec03ab2
rida-123 <ridashoaib666@gmail.com>
2022-05-12 12:56:03 -0700

    responsive

--- src/components/aboutprojects/aboutprojects.js
+++ src/components/aboutprojects/aboutprojects.js
@@ -7,15 +7,16 @@ const AboutProject=()=>{
          <h4 className='livepro'>Live Projects</h4>
          <br></br>
          <br></br>
-         <div className="card" style={{ width: "25rem" }}>
+         
+         <div className="card" style={{ width: "29rem",display:"inline-block"}}>
   <img className="card-img-top" src="fullsize.png" alt="Card image cap" />
   <div className="card-body">
     <p className="card-text">
         <div className='row'>
-            <div className='col-sm-1 col-md-1 col-lg-1'>
+            <div className='col-sm-1 col-md-1 col-lg-1 col-1 '>
             <img src="Line 17.png"></img>
             </div>
-            <div className='col-sm-11 col-md-11 col-lg-11'>
+            <div className='col-sm-11 col-md-11 col-lg-11  col-11 '>
             <button className='btn8'>REGISTRATION CLOSED</button>
                                                    
                  <img src="Capture.PNG " style={{marginLeft:"20px"}}></img>
@@ -33,13 +34,79 @@ const AboutProject=()=>{
                                 <a href="#"><img src="vector4.png"></img></a>
                              
                                 </div>
+        <div className='gradient'>
+          <div className='row'>
+              <div className='col-5 col-sm-5 col-md-5 col-lg-5'>
+                  <p className='gradientp'>FCFS OPEN <br></br>TBA</p>
+              </div>
+              <div className='col-1 col-sm-1 col-md-1 col-lg-1'>
+                  <img src="Line 18.png"></img>
+              </div>
+              <div className='col-5 col-sm-5 col-md-5 col-lg-5' >
+              <p className='gradientp' >FCFS CLOSE <br></br>TBA</p>
+              </div>
+          </div>
+          
+          </div>
+          <p className='aboutWhislist'>Whitelist end in:<br></br>01:04:32:15</p>
+     <center><button className='btn9'>PURCHASE</button></center> 
+          
+          </p>
+          </div>
+        </div>
 
-
-    </p>
+        <div className="card" style={{ width: "29rem",display:"inline-block", marginLeft:"100px"}}>
+  <img className="card-img-top" src="fullsize.png" alt="Card image cap" />
+  <div className="card-body">
+    <p className="card-text">
+        <div className='row'>
+            <div className='col-sm-1 col-md-1 col-lg-1 col-1 '>
+            <img src="Line 17.png"></img>
+            </div>
+            <div className='col-sm-11 col-md-11 col-lg-11  col-11 '>
+            <button className='btn8'>REGISTRATION CLOSED</button>
+                                                   
+                 <img src="Capture.PNG " style={{marginLeft:"20px"}}></img>
+                <span className='avaxtxt'>Avax</span>
+                <h3 className='proHead'>MetaLand</h3>
+                <p className='propara'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything.</p>
+            
+            </div>
+        </div>
+        <div className='icons1'>
+                                <a href="#"><img src="vector1.png"></img></a>
+                                <a href="#"><img src="vector2.png"></img></a>
+                                <a href="#"><img src="vector5.png"></img></a>
+                                <a href="#"><img src="vector3.png"></img></a>
+                                <a href="#"><img src="vector4.png"></img></a>
+                             
+                                </div>
+        <div className='gradient'>
+          <div className='row'>
+              <div className='col-5 col-sm-5 col-md-5 col-lg-5'>
+                  <p className='gradientp'>FCFS OPEN <br></br>TBA</p>
+              </div>
+              <div className='col-1 col-sm-1 col-md-1 col-lg-1'>
+                  <img src="Line 18.png"></img>
+              </div>
+            
+              <div className='col-5 col-sm-5 col-md-5 col-lg-5' >
+              <p className='gradientp' >FCFS CLOSE <br></br>TBA</p>
+              </div>
+          </div>
+          
+          </div>
+          <p className='aboutWhislist'>Whitelist end in:<br></br>01:04:32:15</p>
+     <center><button className='btn9'>PURCHASE</button></center> 
+          
+          </p>
+          </div>
+        </div>
+    
   </div>
-</div>
 
-         </div>
+
+   
         |</>
     )
 }
