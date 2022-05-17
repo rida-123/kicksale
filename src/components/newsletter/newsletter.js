e3b3256b1fb947b81a3926240af751bf222029dc
rida-123 <ridashoaib666@gmail.com>
2022-05-16 17:56:12 -0700

    responsive

--- src/components/newsletter/newsletter.js
+++ src/components/newsletter/newsletter.js
@@ -3,26 +3,21 @@ const Newsletter=()=>{
     return(
         <>
         <div className='news' style={{paddingBottom:'40px'}}>
-            <div className='container' style={{paddingTop:"100px",maxWidth:"1160px"}}>
+            <div className='container ne' style={{paddingTop:"100px",maxWidth:"1160px"}}>
             <div className='row newsletter'>
-                <div className='col-sm-4 col-md-4 col-lg-4' style={{textAlign:"center"}}>
+                <div className='col-sm-12 col-12 col-md-4 col-lg-4' style={{textAlign:"center"}}>
 <img src="ph.png" className='photo'></img>
                 </div>
-                <div className='col-sm-8 col-md-8 col-lg-8'>
+                <div className='col-12 col-sm-12 col-md-8 col-lg-8'>
                     <div className='newss'>
                     <h1 className='newsLetterHeading'>Follow & Subscribe to our Newsletter.</h1>
                     <p>Ready to get started, Get our Newsletter and join the Community!
 </p>
-<div class="input-group mb-3">
-  <input type="text" className="form-control" placeholder="Your Email" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
- 
-  <div class="input-group-append">
 
-      <button class="btn" type="button">Subscribe</button>
-      
-  
-  </div>
-</div>
+<input className="inputfieldss" type="text" placeholder="Email"/>
+
+   <input type="button" value="Subscribe"className="btn19"/>      
+               
                     </div>
       
                     </div>
